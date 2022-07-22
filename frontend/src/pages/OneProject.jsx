6b70333056f0990ead92fc0e4de72dd0304c182d
Alexandre Pillias <alexandre.pillias@gmail.com>
2022-07-22 11:50:32 +0200

    finished

--- frontend/src/pages/OneProject.jsx
+++ frontend/src/pages/OneProject.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import axios from "axios";
-import { Box, Heading } from "@chakra-ui/react";
+import { Box, Heading, Text } from "@chakra-ui/react";
 
 import Header from "../components/Header";
 
@@ -17,11 +17,28 @@ function OneProject() {
   }, []);
 
   return (
-    <Box bgColor="#111111" h="100vh" color="white">
+    <Box
+      bgColor="#111111"
+      h="100vh"
+      color="white"
+      textAlign={{ base: "center", lg: "start" }}
+    >
       <Header />
-      <Heading as="h1" color="white" pt="15rem">
+      <Heading as="h1" pt="5rem" pl={{ base: "0", lg: "5rem" }}>
         You're here to learn more about {oneProject.name}
       </Heading>
+      <Heading as="h2" pl={{ base: "0", lg: "5rem" }} pt="1rem" fontSize="3xl">
+        A small descrition of the project
+      </Heading>
+      <Text pl={{ base: "0", lg: "5rem" }} pt="0.5rem">
+        {oneProject.description}
+      </Text>
+      <Heading as="h3" pl={{ base: "0", lg: "5rem" }} pt="1rem" fontSize="3xl">
+        Link to the github repository :
+      </Heading>
+      <Text pl={{ base: "0", lg: "5rem" }} pt="0.5rem">
+        {oneProject.image}
+      </Text>
     </Box>
   );
 }
